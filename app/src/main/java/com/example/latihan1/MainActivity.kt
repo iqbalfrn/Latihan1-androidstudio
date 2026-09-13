1b5fbedcfba0f3b70dd83c9067c78473392e601b
iqbalfrn <iqbaalllfernan@gmail.com>
2026-09-13 20:22:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.latihan1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.buttonClick)
+
+        buttonClick.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Tombol berhasil diklik!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
